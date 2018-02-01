19bb235dc4bcea69b39564a90c55c119ed2064e9
rexyang <rexyangluck@live.com>
2018-02-01 15:22:47 +0800

    提交

---
+++ video-online/static/dicConfig.js
@@ -0,0 +1,24 @@
+export default  {
+    api: {
+       
+    },
+    codeDic: [
+        { '0': '失败' },
+        { '1': '成功' }
+    ],
+    doMain: {
+        url: 'http://campusimage.zhaopin.com/pic/'
+    },
+    educationDicList :{
+        '博士研究生': '博士',
+        '硕士研究生': '硕士',
+        'MBA-MBA': 'MBA',
+        'MBA': 'MBA',
+        '大学本科': '本科',
+        '大学专科': '专科',
+        '高中': '高中',
+        '初中': '初中',
+        '其他':'其他',
+        '中专/技校/职高': '中专类'
+    }
+}
---
+++ video-online/static/js/4.67242543b394bd216c3b.js
@@ -0,0 +1,2 @@
+webpackJsonp([4],{UmIl:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("h2",[this._v("设备检测")])])}]};var c=n("Z0/y")({created:function(){}},r,!1,function(e){n("n7XL")},"data-v-605eac04",null);t.default=c.exports},n7XL:function(e,t){}});
+//# sourceMappingURL=4.67242543b394bd216c3b.js.map
